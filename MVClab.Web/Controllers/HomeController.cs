9a9cc7fef87af8771ec6332e882c79ab167a4e00
ohaganry <hermits.nature@gmail.com>
2020-02-21 15:45:39 -0500

    initial commit

---
+++ MVClab.Web/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVClab.Web.Models;
+
+namespace MVClab.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Mileage()
+        {
+            return View();
+        }
+
+        public IActionResult MileageResult(double mpg, double gallons, double trip)
+        {
+            double range = gallons * mpg;
+            string result = "";
+
+            if (range < trip)
+            {
+                result = "You need more gas";
+            }
+            else
+            {
+                result = "You have enough gas";
+            }
+            return View("MileageResult", result);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
